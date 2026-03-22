a6f1ffaa56a02431d3d6f66c12e6af7221aeedee
Your Name <freeluck.v.i.p999@gmail.com>
2026-03-22 00:39:16 +0000

    making progress with constant function but code is not execuatble state yet

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int tokenize(string myString, string strArray[NUM_OF_TOKENS]){
 }
 
 
+void consonant(string [], int);
 int main ()
 {
       string str = " "; // string is character array
@@ -47,11 +48,39 @@ int main ()
            
 
 	// TODO: call a function to encrypt strings that starts with a consonant
+    consonant(arr, tsize);
 
 	// TODO: call a function to encrypt short strings
     
 	// TODO: display the encrypted strings (the results)
+    for(int i = 0; i < tsize; i++){
+     cout << arr[i] << endl;
+    }
 
     return 0; 
+}
+
+
+void consonant(string a[], int s){ // array of strings (array within an array) and num of tokens
+int tmp = 0; //tmp variable
+int toklen = 0; //token length 
+for(int i = 0; i < s; i++){ // checking the entire token string array
+ for(int j = 0; j < a[i].length(); j++){ // checking against each individual token string
+  toklen = a[i].length();
+  // a[i][j] means character now cuz we in a string(array) in a array
+  if(a[i][0] != 'a' && a[i][0] != 'A' && a[i][0] != 'e' && a[i][0] != 'E' && a[i][0] != 'o' && a[i][0] != 'O' && a[i][0] != 'u' && a[i][0] != 'U')
+  {
+    // if not vowel swap to move to end of token for first 3 letters
+    //for(int k = 0; k < 3; k++){
+     for(int l = 0; l < toklen - 1; l++){ 
+      tmp = a[i][l];
+      a[i][l] = a[i][l + 1];
+      a[i][l + 1] = tmp;
+     }
+    //}
+  }
+
+ }
+}
 }
 
